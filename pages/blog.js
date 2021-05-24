4297adccb0d305ddaecd5a9082effff864be6a0e
Juan <ks@Juans-MacBook-Pro.local>
2021-05-24 18:59:25 -0400

    blog

--- pages/blog.js
+++ pages/blog.js
@@ -6,15 +6,13 @@ import getPosts from "@utils/getPosts";
 const Index = ({ posts, title, description, ...props }) => {
   return (
     <Layout pageTitle={title} description={description}>
-      <section className="w-screen items-left px-12">
-        <section className="w-full items-left ">
-          <Link href="/">
-            <span className="font-bold text-4xl text-gray-800 dark:text-red-50">
-              Juan M. Rada
-            </span>
-          </Link>{" "}
-        </section>
-      </section>
+      <div className="w-screen items-left px-12">
+        <Link href="/">
+          <span className="font-bold text-4xl text-gray-800 dark:text-red-50">
+            Juan M. Rada
+          </span>
+        </Link>{" "}
+      </div>
       <PostList posts={posts} />
     </Layout>
   );

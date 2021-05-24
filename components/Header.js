cf0d75d2ff76be2bda2a1eb99504ed631dcba957
Juan <ks@Juans-MacBook-Pro.local>
2021-05-24 18:07:09 -0400

    nav

--- components/Header.js
+++ components/Header.js
@@ -4,21 +4,15 @@ export default function Header() {
   return (
     <>
       <header className="bg-white dark:bg-gray-900 sticky top-0 z-50 py-4">
-        <nav>
-          <div className="max-w-full px-4 mx-auto space-x-8">
-            <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-              <div class="flex-shrink-0 flex items-center">
-                <Link href="/">
-                  <span className="text-2xl text-purple-500 px-2/4">
-                    Juan M. Rada
-                  </span>
-                </Link>
-              </div>
-              <div className="px-32 float-right absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <ThemeSwitch />
-              </div>
-            </div>
+        <nav class="fixed z-10 flex justify-between w-screen bg-white xl:bg-transparent 2xl:bg-transparent dark:bg-gray-900 xl:dark:bg-transparent 2xl:dark:bg-transparent">
+          <div class="flex flex-row items-center p-4">
+            <ul class="flex items-center"></ul>
           </div>
+          <ul class="flex items-center p-4">
+            <li>
+              <ThemeSwitch />
+            </li>
+          </ul>
         </nav>
       </header>
     </>

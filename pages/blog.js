012eff5e7bc69ea9742d21bda767944294cdd9c3
Juan <ks@Juans-MacBook-Pro.local>
2021-05-24 19:37:42 -0400

    blog

--- pages/blog.js
+++ pages/blog.js
@@ -6,25 +6,31 @@ import getPosts from "@utils/getPosts";
 const Index = ({ posts, title, description, ...props }) => {
   return (
     <Layout pageTitle={title} description={description}>
-      <section className="w-full px-12 box-content h-32 ">
-        <section className="w-full items-left ">
-          <div className="py-24">
-            <Link href="/">
-              <a className="text-4xl text-purple-500 font-bold">
-                Juan M. Rada 🥷
-              </a>
-            </Link>
-            <div className="text-5lg text-left text-gray-800 dark:text-gray-100 font-semibold">
-              ←{" "}
-              <Link href="/">
-                <a>Back to 🏠</a>
-              </Link>
-            </div>
-          </div>
+      <main className="text-gray-800 dark:text-red-50 container mx-auto lg:py-2 xl:py-2 lg:h-screen">
+        <div className="container mx-auto relative max-w-xl">
+          <div className="flex flex-col items-left">
+            <section className="w-full px-12 box-content h-32 ">
+              <section className="w-full items-left ">
+                <div className="py-24">
+                  <Link href="/">
+                    <a className="text-4xl text-purple-500 font-bold">
+                      Juan M. Rada 🥷
+                    </a>
+                  </Link>
+                  <div className="text-5lg text-left text-gray-800 dark:text-gray-100 font-semibold">
+                    ←{" "}
+                    <Link href="/">
+                      <a>Back to 🏠</a>
+                    </Link>
+                  </div>
+                </div>
 
-          <PostList posts={posts} />
-        </section>
-      </section>
+                <PostList posts={posts} />
+              </section>
+            </section>
+          </div>
+        </div>
+      </main>
     </Layout>
   );
 };

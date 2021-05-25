453ffba3f0f5e185ae1d8beb2c963d1446752438
Juan <ks@Juans-MacBook-Pro.local>
2021-05-25 16:38:44 -0400

    revert3

--- pages/post/[postname].js
+++ pages/post/[postname].js
@@ -18,7 +18,10 @@ export default function BlogPost({ siteTitle, frontmatter, markdownBody }) {
           description={`Author: ${frontmatter.author} Date:${frontmatter.date} Description:${frontmatter.description} `}
         />
         <Head>
-          <meta name="Juan M. Rada" content="Juan M. Rada blog post" />
+          <meta
+            name={`Juan M. Rada Blog - Post: ${frontmatter.title}`}
+            content={`Author: ${frontmatter.author} Date:${frontmatter.date} Description:${frontmatter.description} `}
+          />
         </Head>
         <div className="text-xl text-left text-gray-800 dark:text-gray-100 font-bold py-16">
           ←{" "}

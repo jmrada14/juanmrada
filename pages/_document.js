5f46189846ec65d38575e291a5f29148318e34f4
Juan <ks@Juans-MacBook-Pro.local>
2021-05-25 17:06:44 -0400

    revert3

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="public/favicon.ico" />
+          <meta name="Juan M. Rada" content={description}></meta>
         </Head>
         <link rel="shortcut icon" href="/favicon.ico" />
         <body className="bg-white dark:bg-black text-white dark:text-black">

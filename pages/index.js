d16b79bc163b5223c0b21e7440df438ffce97fd0
Juan <ks@Juans-MacBook-Pro.local>
2021-05-26 15:09:43 -0400

    fix darkmode accesibility

--- pages/index.js
+++ pages/index.js
@@ -43,18 +43,18 @@ const Index = () => {
             <br />
             <div className="flex flex-wrap justify-left text-lg">
               <Link href="/blog">
-                <span className="px-2 py-2 font-semibold text-lg text-purple-600 cursor-pointer hover:underline">
+                <span className="px-2 py-2 font-semibold text-lg text-purple-600 dark:text-purple-400 cursor-pointer hover:underline">
                   Blog
                 </span>
               </Link>
               <Link href="https://github.com/jmrada14">
-                <span className="py-2 font-semibold text-lg text-purple-600 cursor-pointer hover:underline">
+                <span className="py-2 font-semibold text-lg text-purple-600 dark:text-purple-400 cursor-pointer hover:underline">
                   GitHub
                 </span>
               </Link>
 
               <Link href="https://www.linkedin.com/in/juan-rada-4b997b190/">
-                <span className="px-2 py-2 font-semibold text-lg text-purple-600 cursor-pointer hover:underline">
+                <span className="px-2 py-2 font-semibold text-lg text-purple-600 dark:text-purple-400 cursor-pointer hover:underline">
                   LinkedIn
                 </span>
               </Link>

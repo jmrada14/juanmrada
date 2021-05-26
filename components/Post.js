55a9c59459619b2b3f48ebd849a4f3db64bea215
Juan <ks@Juans-MacBook-Pro.local>
2021-05-26 15:06:00 -0400

    fix blogpost accesibility

--- components/Post.js
+++ components/Post.js
@@ -58,7 +58,10 @@ export default function Post({
           </div>
         </div>
         <Link href={{ pathname: postHref }}>
-          <a aria-label="read-more-url" className="text-blue-600 text-lg">
+          <a
+            aria-label="read-more-url"
+            className="text-blue-600 dark:text-blue-500 text-lg"
+          >
             Read me
             <svg
               className="inline h-5 w-4 align-text-top"
